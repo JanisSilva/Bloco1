1d128e13d316f91d04177c07804bbde84c2f4e8a
Janis <janis.alves1@gmail.com>
2022-06-07 20:05:08 -0300

    novos exercicios de Java

---
+++ Bloco1/Java/Lista1Java/src/Exercicios/exercicio1.java
@@ -0,0 +1,33 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+/*Faça um programa que receba três inteiros e diga qual deles é o maior.*/
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int num1, num2, num3, maiornum;
+		
+		System.out.print("Digite o 1 número inteiro: ");
+		num1 = entrada.nextInt();
+		
+		System.out.print("Digite o 2 número inteiro: ");
+		num2 = entrada.nextInt();
+		
+		System.out.print("Digite o 3 número inteiro: ");
+		num3 = entrada.nextInt();
+		
+		
+				
+	
+		   {			
+		}
+		
+		
+	}
+
+}
